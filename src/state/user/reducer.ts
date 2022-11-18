0b6a9e051260b18b032360e5cc7267a23da204a5
huanniangstudio <891863891@qq.com>
2022-11-18 11:44:33 +0800

    Set darkmode as default theme

--- src/state/user/reducer.ts
+++ src/state/user/reducer.ts
@@ -64,7 +64,7 @@ function pairKey(token0Address: string, token1Address: string) {
 export const initialState: UserState = {
   selectedWallet: undefined,
   matchesDarkMode: false,
-  userDarkMode: null,
+  userDarkMode: true,
   userExpertMode: false,
   userLocale: null,
   userClientSideRouter: false,
